08c388b7e40a11439920b42700479261b9c4a08b
Daniel García Moreno <dani@danigm.net>
2020-02-14 13:02:39 +0100

    lambda-services: Add collect email first function
    https://phabricator.endlessm.com/T29336

--- lambda-services/functions/collect-email/index.js
+++ lambda-services/functions/collect-email/index.js
@@ -1,4 +1,35 @@
-exports.handler = (event, context, callback) => {
-    // TODO: store the email in the dynamodb
-    callback(null, event);
-};
+const AWS = require('aws-sdk');
+
+const TABLE = process.env.table;
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+function resp(code, data) {
+    return {
+        statusCode: code,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    };
+}
+
+exports.handler = async (event) => {
+    let email;
+
+    try {
+        ({email} = JSON.parse(event.body));
+    } catch(e) {
+        return resp(400, { error: 'Invalid params' });
+    }
+
+    await dynamoDb.put({
+        TableName: TABLE,
+        Item: {
+            email,
+            // We can add here random data, like the form that sent this, the device, etc
+        },
+    }).promise();
+
+    return resp(200, { response: 'ok' });
+}
