6197e2db9e231ac18a8310c8607b988b9efac830
Daniel García Moreno <dani@danigm.net>
2020-03-04 17:23:27 +0100

    toolbox: Move iframe update and proxy to tools
    This patch creates generic functions in toolbox/tools to update a js
    object inside an iframe app to set a proxy to do something on every
    iframe app object change.
    
    With this we'll be able to do something similar for different apps with
    different state in a global variable.
    
    https://phabricator.endlessm.com/T29374

--- src/ui/test/toolbox.test.jsx
+++ src/ui/test/toolbox.test.jsx
@@ -13,6 +13,7 @@ import QuestFTHView from '../quest-fth-view';
 
 import Toolbox from './fizzics-toolbox';
 import { actions } from '../../store';
+import { proxyApp, updateApp } from '../toolbox/tools';
 
 const useStyles = makeStyles({
   root: {
@@ -28,80 +29,24 @@ const App = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
-  // Dispatch the action to update the gameState in the store and also updates
-  // the iframe globalParameters to reflect toolbox changes in the game
-  const updateApp = (newParams) => {
-    const app = document.querySelector('#app');
-    if (app.contentWindow && app.contentWindow.globalParameters) {
-      const { globalParameters } = app.contentWindow;
-
-      Object.keys(newParams).forEach((property) => {
-        if (!(property in globalParameters)) {
-          return;
-        }
-        const value = newParams[property];
-        if (value === globalParameters[property]) {
-          return;
-        }
-        globalParameters[property] = value;
-        dispatch(actions.gameSetParam(property, value));
-      });
-    }
+  const update = (newParams) => {
+    // update the iframe with newParams
+    updateApp('globalParameters', newParams, (property, value) => {
+      dispatch(actions.gameSetParam(property, value));
+    });
   };
 
-  // Creates a proxy to track iframe globalParameters
   useEffect(() => {
-    const app = document.querySelector('#app');
-    let proxy = null;
-    let globalParameters = null;
-
-    const proxyApp = () => {
-      if (!app.contentWindow.globalParameters) {
-        setTimeout(proxyApp, 500);
-        return;
-      }
-
-      if (proxy) {
-        return;
-      }
-
-      let timeout = null;
-      const updateToolbox = () => {
-        dispatch(actions.gameSet(globalParameters));
-      };
-
-      const handler = {
-        set(obj, prop, val) {
-          // Timeout update to just trigger one update if there are multiple changes
-          if (timeout) {
-            clearTimeout(timeout);
-          }
-          timeout = setTimeout(updateToolbox, 300);
-
-          return Reflect.set(obj, prop, val);
-        },
-      };
-
-      globalParameters = app.contentWindow.globalParameters;
-      proxy = new Proxy(globalParameters, handler);
-      app.contentWindow.globalParameters = proxy;
-
-      dispatch(actions.gameSet(globalParameters));
+    const changeCallback = (params) => {
+      dispatch(actions.gameSet(params));
     };
 
-    proxyApp();
-
-    const cleanup = () => {
-      if (globalParameters) {
-        app.contentWindow.globalParameters = globalParameters;
-      }
-    };
-
-    return cleanup;
+    // Creates a proxy to track iframe globalParameters
+    proxyApp('globalParameters', changeCallback);
   });
 
   const toolbox = (
-    <Toolbox updateApp={updateApp} />
+    <Toolbox updateApp={update} />
   );
 
   const canvas = (
