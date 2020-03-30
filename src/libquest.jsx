endlessm/hack-web

commit b9ce41072bee7c36b626a0baa316c14807b777eb

libquest: Add more wait for options
This patch adds a series of regular expressions that will allow some
variable checking in quests:

 * wait for: VARIABLE contains "text"
 * wait for: VARIABLE not contains "text"
 * wait for: VARIABLE is value
 * wait for: VARIABLE is not value

There's also a new test quest to show how to use this new functionality,
that can be tested uncommenting the line in src/index.jsx:

// import App from './ui/test/html-quest.test';

https://phabricator.endlessm.com/T29640

src/libquest.jsx
@@ -23,7 +23,57 @@ const extractLineCharacter = (currentTags) => (
 const extractWaitForVariables = (choiceText) => {
   const result = choiceText.match(/\(wait for: (.*)\)/);
   if (!result) return [];
-  return result[1].split(' ');
+
+  const checks = [
+    {
+      // wait for: code not contains "test"
+      match: /^([^ ]+) not contains "(.*)"$/,
+      fn: (variable, str) => !variable.includes(str),
+    },
+    {
+      // wait for: code contains "test"
+      match: /^([^ ]+) contains "(.*)"$/,
+      fn: (variable, str) => variable.includes(str),
+    },
+    {
+      // wait for: code is not 12
+      match: /^([^ ]+) is not (.*)$/,
+      fn: (variable, value) => {
+        if (value === 'true') {
+          return !Boolean(variable);
+        }
+        if (value === 'false') {
+          return Boolean(variable);
+        }
+
+        return String(variable) !== value;
+      },
+    },
+    {
+      // wait for: code is 12
+      match: /^([^ ]+) is (.*)$/,
+      fn: (variable, value) => {
+        if (value === 'true') {
+          return Boolean(variable);
+        }
+        if (value === 'false') {
+          return !Boolean(variable);
+        }
+
+        return String(variable) === value;
+      },
+    },
+  ];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const c of checks) {
+    const match = result[1].match(c.match);
+    if (match) {
+      return [{ name: match[1], fn: c.fn, params: match.slice(2) }];
+    }
+  }
+
+  return result[1].split(' ').map((r) => ({ name: r }));
 };
 
 export default class Quest {
@@ -77,7 +127,11 @@ export default class Quest {
         choices = [...choices, c];
       }
       waitForVariables.forEach((variable) => {
-        this.waitFor = { ...this.waitFor, ...{ [variable]: c } };
+        const waitForContext = {
+          choice: c,
+          variable,
+        };
+        this.waitFor = { ...this.waitFor, [variable.name]: waitForContext };
       });
     });
 
@@ -91,7 +145,12 @@ export default class Quest {
   doUpdateStoryVariable(name, newValue) {
     this.story.variablesState[name] = newValue;
     if (name in this.waitFor) {
-      this.choose(this.waitFor[name]);
+      const { choice, variable } = this.waitFor[name];
+      const fn = variable.fn || (() => true);
+      const params = variable.params || [];
+      if (fn.apply(null, [newValue, ...params])) {
+        this.choose(choice);
+      }
     }
   }
 

src/index.jsx
@@ -13,6 +13,7 @@ import App from './ui/app';
 // import App from './ui/test/dialogue.test';
 // import App from './ui/test/toolbox.test';
 // import App from './ui/test/p5.test';
+// import App from './ui/test/html-quest.test';
 
 render(
   <Provider store={store}>

src/ui/test/html-quest.test.jsx
@@ -1,0 +1,132 @@
+import { hot } from 'react-hot-loader';
+import React, { useEffect, useState } from 'react';
+import {
+  Typography,
+  Grid,
+} from '@material-ui/core';
+
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/theme-terminal';
+
+import TestWrapper from './test-wrapper';
+import Dialogue from '../dialogue';
+import QuestFTHView from '../quest-fth-view';
+import Quest from '../../libquest';
+import questContent from './html1-quest.ink';
+
+const App = () => {
+  const [quest] = useState(new Quest(questContent));
+
+  const [dialogue, setDialogue] = useState([]);
+  const [choices, setChoices] = useState([]);
+  const [currentChoice, setCurrentChoice] = useState(null);
+
+  useEffect(() => {
+    // Initial setup of dialogue and choices.
+
+    if (quest === undefined) return;
+
+    const { dialogue: dia, choices: cho } = quest.continueStory();
+    setDialogue((oldDialogue) => [...oldDialogue, ...dia]);
+    setChoices(cho);
+  }, [quest]);
+
+  useEffect(() => {
+    // Update dialogue and choices when a choice is selected.
+
+    if (quest === undefined) return;
+    if (currentChoice === null) return;
+
+    if (currentChoice !== undefined) {
+      quest.choose(choices[currentChoice.index]);
+    }
+
+    // FIXME this is duplicated
+    const { dialogue: dia, choices: cho } = quest.continueStory();
+    setDialogue((oldDialogue) => [...oldDialogue, ...dia]);
+    setChoices(cho);
+
+    setCurrentChoice(null);
+  }, [quest, choices, currentChoice]);
+
+  const [state, setState] = useState(`
+<html> <!-- This tag tells the browser that this is an HTML document. That's a fancy way of saying it's a web page. -->
+  <head> <!-- The <head> element is the header of the page. It contains everything that shows up at the very beginning of the page, like the page number and chapter name in a book. -->
+  <title>This is a Test Web Page!</title>
+  </head>
+  <body> <!-- The <body> tag is what most people think of as the 'web page'.  <body> contains all the content you normally see, like text, links, and images. -->
+  <h1>Welcome to my test website!</h1>
+  <p>Hello, world!</p>
+  </body> <!-- This is the closing tag for <body>. -->
+</html> <!-- This is the closing tag for <html>, which means this is the end of the webpage. -->
+`);
+
+  useEffect(() => {
+    quest.updateStoryVariable('code', state);
+    setCurrentChoice(undefined);
+  }, [state, quest]);
+
+  const handleFlipped = (flipped) => {
+    quest.updateStoryVariable('flipped', flipped);
+    setCurrentChoice(undefined);
+  };
+
+  const handleChoiceSelected = (choice) => {
+    setCurrentChoice(choice);
+  };
+
+  const toolbox = (
+    <div>
+      <Typography variant="h4">This is the toolbox</Typography>
+    </div>
+  );
+
+  const sidebar = (
+    <Dialogue
+      dialogue={dialogue}
+      choices={choices}
+      onChoiceSelected={handleChoiceSelected}
+    />
+  );
+
+  const style = {
+    width: '100%',
+    height: '100vh',
+    border: 0,
+  };
+
+  const canvas = (
+    <Grid container>
+      <Grid item xs={6}>
+        <AceEditor
+          width="100%"
+          height="98vh"
+          mode="html"
+          theme="terminal"
+          value={state}
+          onChange={setState}
+          name="editor"
+          editorProps={{ $blockScrolling: true }}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <iframe srcDoc={state} style={style} title="html" />
+      </Grid>
+    </Grid>
+  );
+
+
+  return (
+    <TestWrapper>
+      <QuestFTHView
+        toolbox={toolbox}
+        canvas={canvas}
+        sidebar={sidebar}
+        onFlipped={handleFlipped}
+      />
+    </TestWrapper>
+  );
+};
+
+export default hot(module)(App);

src/ui/test/html1-quest.ink
@@ -1,0 +1,47 @@
+# main character: riley
+
+VAR username = "hacker"
+VAR flipped = false
+VAR code = ""
+
+-> begin
+
+=== begin ===
+- Hi, {username}! I know you've used the internet before - but have you ever wondered what goes into showing you all those web pages?
+- I was wondering that, one day, and I decided to go find out. There was a ton to learn, but now that I've done the legwork, I can pass it on to you.
+- OK, first, let's get you looking at the guts of a web page.
++ [❯] Okay
+  -> in_html
+
+=== in_html ===
+
+- Here we are! One half of your browser shows you the code that makes up this webpage, and the other half shows what you'd see if you found this page on the internet.
+- Just like computer programs, web pages are written in a kind of code - It's called HTML.
+- You put HTML around text, and that tells your web browser what to show, and how to show it on your screen. Not so bad, right?
++ [❯] Tell me more.
+  -> in_html2
+
+=== in_html2 ===
+
+- In HTML, everything with < and > around it is called a tag. Tags tell the browser how to display something - text, images, whatever.
+- Tags come in pairs - an <opening tag>, and a </closing tag>, like saying start and stop. Notice the "/" on the closing tag!
+- You'll also see some text with '<!--' and '--\>' around it. Those are called comments, and the web browser won't look at them - they're info I put there to explain what some of the HTML does.
+* [❯] A lot of theory...
+  -> in_html3
+
+=== in_html3 ===
+
+- Let's get our hands dirty! Find the text Hello, World! In the code part of this page, and change it - make it your name, your favorite food, whatever you like.
+* [(wait for: code not contains "Hello, world!")](Done)
+  -> in_html4
+
+=== in_html4 ===
+
+- Awesome! You just edited a webpage! That makes you a web designer, and that's pretty cool.
+- Let's take it one step further. Do you see the text that says Welcome near the top of the webpage? Try finding that text in the code area and changing it.
+* [(wait for: code not contains "Welcome to my test website")](Done)
+  -> success
+
+=== success ===
+Yes! You did it!
+-> END

test/libquest.test.jsx
@@ -84,7 +84,7 @@ describe('libquest', () => {
     expect(Object.keys(quest.waitFor).length).toEqual(0);
     const { choices } = quest.continueStory();
     expect(choices.length).toEqual(1);
-    expect(Object.keys(quest.waitFor).length).toEqual(2);
+    expect(Object.keys(quest.waitFor).length).toEqual(7);
     expect('flipped' in quest.waitFor).toBe(true);
     expect('radius' in quest.waitFor).toBe(true);
     expect('wrong_variable' in quest.waitFor).toBe(false);
@@ -105,8 +105,53 @@ describe('libquest', () => {
     quest.continueStory();
     quest.updateStoryVariable('radius', 85);
     expect(spyOnChoose).toHaveBeenCalledTimes(2);
-    const { dialogue: newDialogue } = quest.continueStory();
+    let { dialogue: newDialogue } = quest.continueStory();
     expect(newDialogue[0].text).toEqual('something changed! radius: 85 flipped: 1\n');
+
+    // Now we go back to the same step, to test contains:
+    quest.story.ChoosePathString('step_a');
+    quest.continueStory();
+    quest.updateStoryVariable('filter', 'this is a test filter string');
+    expect(spyOnChoose).toHaveBeenCalledTimes(3);
+    ({ dialogue: newDialogue } = quest.continueStory());
+    expect(newDialogue[0].text).toEqual('something changed! filter: this is a test filter string filter2: sample text\n');
+    quest.updateStoryVariable('filter', 'sample text');
+
+    quest.story.ChoosePathString('step_a');
+    quest.continueStory();
+    quest.updateStoryVariable('filter2', 'real text');
+    expect(spyOnChoose).toHaveBeenCalledTimes(4);
+    ({ dialogue: newDialogue } = quest.continueStory());
+    expect(newDialogue[0].text).toEqual('something changed! filter: sample text filter2: real text\n');
+    quest.updateStoryVariable('filter2', 'sample text');
+
+    // Now we go back to the same step, to test is:
+    quest.story.ChoosePathString('step_a');
+    quest.continueStory();
+    quest.updateStoryVariable('number', 27);
+    expect(spyOnChoose).toHaveBeenCalledTimes(4);
+    quest.updateStoryVariable('number', 28);
+    expect(spyOnChoose).toHaveBeenCalledTimes(5);
+    ({ dialogue: newDialogue } = quest.continueStory());
+    expect(newDialogue[0].text).toEqual('something changed! number: 28 number2: 0\n');
+    quest.updateStoryVariable('number', 0);
+
+    quest.story.ChoosePathString('step_a');
+    quest.continueStory();
+    quest.updateStoryVariable('number2', 28);
+    expect(spyOnChoose).toHaveBeenCalledTimes(6);
+    ({ dialogue: newDialogue } = quest.continueStory());
+    expect(newDialogue[0].text).toEqual('something changed! number: 0 number2: 28\n');
+    quest.updateStoryVariable('number2', 0);
+
+    quest.story.ChoosePathString('step_a');
+    quest.continueStory();
+    quest.updateStoryVariable('finished', false);
+    expect(spyOnChoose).toHaveBeenCalledTimes(6);
+    quest.updateStoryVariable('finished', true);
+    expect(spyOnChoose).toHaveBeenCalledTimes(7);
+    ({ dialogue: newDialogue } = quest.continueStory());
+    expect(newDialogue[0].text).toEqual('something changed! finished: 1\n');
 
     spyOnChoose.mockRestore();
   });

test/quest.ink
@@ -2,6 +2,11 @@
 
 VAR radius = 30
 VAR flipped = false
+VAR filter = "sample text"
+VAR number = 0
+VAR filter2 = "sample text"
+VAR number2 = 0
+VAR finished = false
 
 -> step_a
 
@@ -10,9 +15,26 @@ This is the first line
 This is the second line # character: riley
 + Bye -> step_end
 + [(wait for: flipped radius)] -> variables_changed
++ [(wait for: filter contains "test filter")] -> filter_changed
++ [(wait for: filter2 not contains "sample")] -> filter_changed
++ [(wait for: number is 28)] -> number_changed
++ [(wait for: number2 is not 0)] -> number_changed
++ [(wait for: finished is true)] -> finished_changed
 
 == variables_changed
 something changed! radius: {radius} flipped: {flipped}
+-> END
+
+== filter_changed
+something changed! filter: {filter} filter2: {filter2}
+-> END
+
+== number_changed
+something changed! number: {number} number2: {number2}
+-> END
+
+== finished_changed
+something changed! finished: {finished}
 -> END
 
 == step_end

commit ac3e3e059ff11a95699cbd82fc58f879e214ff26

libquest: Fix lint errors
They were introduced in commit b9ce4107 .

src/libquest.jsx
@@ -41,7 +41,7 @@ const extractWaitForVariables = (choiceText) => {
       match: /^([^ ]+) is not (.*)$/,
       fn: (variable, value) => {
         if (value === 'true') {
-          return !Boolean(variable);
+          return !variable;
         }
         if (value === 'false') {
           return Boolean(variable);
@@ -58,7 +58,7 @@ const extractWaitForVariables = (choiceText) => {
           return Boolean(variable);
         }
         if (value === 'false') {
-          return !Boolean(variable);
+          return !variable;
         }
 
         return String(variable) === value;