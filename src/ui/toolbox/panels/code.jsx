96f309d517312ae8f9e8cad05ff496beac2d8932
Manuel Quiñones <manuel.por.aca@gmail.com>
2020-04-28 10:40:24 -0300

    Toolbox code: Add full height setting
    When this flag is set, a hook will be used to resize the code editor
    according to the window size.
    
    https://phabricator.endlessm.com/T29943

--- src/ui/toolbox/panels/code.jsx
+++ src/ui/toolbox/panels/code.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  useTheme,
+} from '@material-ui/core';
 
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-java';
@@ -8,14 +12,39 @@ import 'ace-builds/src-noconflict/theme-monokai';
 
 import { actions } from '../../../store';
 
+// Hook
+function useWindowSize() {
+  function getSize() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
+
 const CodePanel = ({
   code,
   compile,
   buildDelay,
+  fullHeight,
 }) => {
   const params = useSelector((state) => state.hackableApp);
   const dispatch = useDispatch();
   const text = code(params);
+  const size = useWindowSize();
 
   let timeout = null;
   const delayBuild = (c) => {
@@ -41,9 +70,13 @@ const CodePanel = ({
     timeout = setTimeout(() => delayBuild(c), 1000);
   };
 
+  const theme = useTheme();
+  const editorHeight = fullHeight ? `${size.height - theme.spacing(10)}px` : undefined;
+
   return (
     <AceEditor
       width="100%"
+      height={editorHeight}
       mode="javascript"
       theme="monokai"
       value={text}
@@ -57,10 +90,12 @@ CodePanel.propTypes = {
   code: PropTypes.func.isRequired,
   compile: PropTypes.func.isRequired,
   buildDelay: PropTypes.number,
+  fullHeight: PropTypes.bool,
 };
 
 CodePanel.defaultProps = {
   buildDelay: 1000,
+  fullHeight: false,
 };
 
 export default CodePanel;
