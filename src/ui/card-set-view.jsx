endlessm/hack-web

commit e2551cd12fadfee52e6c054844638cf0d94f319a

cards: Remove desktop references from main cards - Add empty state wording
https://phabricator.endlessm.com/T29903

src/ui/card-set-view.jsx
@@ -71,8 +71,8 @@ const CardSetView = ({ slug }) => {
   const emptyContent = (
     <Grid container justify="flex-start">
       <Grid item>
-        <Typography variant="h5">Empty Content</Typography>
-        <Typography variant="body1">Empty Description</Typography>
+        <Typography variant="h5">Hey, Hacker!</Typography>
+        <Typography variant="body1">My name's Riley, and I'm here to show off Endless OS and Hack! Pick a card and check out what we've got to offer!</Typography>
       </Grid>
     </Grid>
   );

src/store.jsx
@@ -135,13 +135,13 @@ const initialState = {
           slug: '/maker',
           name: 'Engineering',
           subtitle: 'Learn how strong buildings stay up!',
-          description: 'Learn some basic engineering in this fun (and tasty!) activity. This activity is just one of the many real-world activities the desktop version of Hack offers - the virtual world is useful, but there is no substitute for building with your own hands!',
+          description: 'Learn some basic engineering in this fun (and tasty!) activity. The virtual world is useful, but there is no substitute for building with your own hands!',
         },
         {
           slug: '/art',
           name: 'Processing',
           subtitle: 'Take your first steps into programming!',
-          description: 'Processing is a language that connects programming to art, making it easy for anyone to understand and start learning. The desktop version of Hack includes more activities that ease you into Javascript, a common programming language with many different uses.',
+          description: 'Processing is a language that connects programming to art, making it easy for anyone to understand and start learning. Check out the bascis of Javascript, a common programming language with many different uses!',
         },
         {
           slug: '/os',

commit 00440488f0b1638a6588964202c0edfa2b924417

UI: Fix lint errors introduced in e2551cd1
https://phabricator.endlessm.com/T29903

src/ui/card-set-view.jsx
@@ -72,7 +72,7 @@ const CardSetView = ({ slug }) => {
     <Grid container justify="flex-start">
       <Grid item>
         <Typography variant="h5">Hey, Hacker!</Typography>
-        <Typography variant="body1">My name's Riley, and I'm here to show off Endless OS and Hack! Pick a card and check out what we've got to offer!</Typography>
+        <Typography variant="body1">My name&apos;s Riley, and I&apos;m here to show off Endless OS and Hack! Pick a card and check out what we&apos;ve got to offer!</Typography>
       </Grid>
     </Grid>
   );