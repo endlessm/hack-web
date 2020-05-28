b3043e9b5c7d494891b6a3b3e945ff8630c3b780
Daniel García Moreno <dani@danigm.net>
2020-05-28 10:25:06 +0200

    ui: Add SecondaryIconButton with custom styles
    The SecondaryIconButton is just an IconButton with the blue background
    and white icon. On hover it goes blue background and hack green icon.
    
    https://phabricator.endlessm.com/T30238

--- src/ui/main-button.jsx
+++ src/ui/main-button.jsx
@@ -99,6 +99,27 @@ const MainIconButton = AttractWrapper(withStyles(({
   },
 }))(IconButton));
 
+const SecondaryIconButton = AttractWrapper(withStyles(({
+  palette, shadows, spacing, transitions,
+}) => ({
+  root: {
+    boxShadow: shadows[2],
+    transition: transitions.create(['box-shadow', 'color', 'background'], {
+      easing: transitions.easing.easeInOut,
+      duration: transitions.duration.short,
+    }),
+    color: palette.secondary.contrastText,
+    background: palette.secondary.main,
+    borderRadius: spacing(3),
+    whiteSpace: 'nowrap',
+    '&:hover': {
+      boxShadow: shadows[4],
+      color: palette.primary.main,
+      background: palette.secondary.main,
+    },
+  },
+}))(IconButton));
+
 const GoButton = AttractWrapper(({ card }) => {
   const dispatch = useDispatch();
 
@@ -143,5 +164,5 @@ GoButton.propTypes = {
 };
 
 export {
-  MainButton, MainIconButton, GoButton,
+  MainButton, MainIconButton, GoButton, SecondaryIconButton,
 };
--- src/ui/reload-button.jsx
+++ src/ui/reload-button.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Fab, Tooltip } from '@material-ui/core';
+import { Tooltip } from '@material-ui/core';
 import { SettingsBackupRestore } from '@material-ui/icons';
 import { useTranslation } from 'react-i18next';
 
-const ReloadButton = ({ onClick, color }) => {
+import { SecondaryIconButton } from './main-button';
+
+const ReloadButton = ({ onClick }) => {
   const { t } = useTranslation();
 
   return (
     <Tooltip title={t('Reset code')}>
-      <Fab
-        color={color}
+      <SecondaryIconButton
         aria-label={t('Reset code')}
         edge="end"
         size="medium"
         onClick={onClick}
       >
         <SettingsBackupRestore />
-      </Fab>
+      </SecondaryIconButton>
     </Tooltip>
   );
 };
 
 ReloadButton.propTypes = {
   onClick: PropTypes.func,
-  color: PropTypes.string,
 };
 
 ReloadButton.defaultProps = {
   onClick: null,
-  color: 'secondary',
 };
 
 export default ReloadButton;
